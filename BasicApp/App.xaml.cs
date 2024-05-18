c392179703d3d72f0a26dffb14ac9bc693375a82
Richard Negron <richard.negron.257@gmail.com>
2024-05-18 10:46:38 -0500

    Add project files.

---
+++ BasicApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BasicApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ BasicApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
